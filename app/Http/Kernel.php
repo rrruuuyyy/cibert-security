21793abd4cf4a5019c02e902d92c68ca46aa5fb0
Rodrigo Mendoza Cabrera <ruymenca1@gmail.com>
2020-04-21 12:53:28 -0500

    Toen deteckt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+        // 'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth' => \App\Http\Middleware\CheckApiToken::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
